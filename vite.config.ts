a334cab62a63f3a013511863c82d62e71522ab22
Dan Kolesnikov <kolesnikov43452@gmail.com>
2025-01-30 23:01:56 +0300

    base removed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tc-react',
+  // base: '',
   plugins: [react(), tsconfigPaths()],
 })
